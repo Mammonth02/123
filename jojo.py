d32df282112ab09b79da9c226960be6d7b7843b0
Mammonth02 <mammonth02@gmail.com>
2022-03-09 15:50:54 +0600

    ass

---
+++ jojo.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+sss = []
+for i in range(1, 10):
+    url = f'https://shop.casio.ru/catalog/?PAGEN_1={i}'
+    req = requests.get(url).text
+    soup = BeautifulSoup(req, 'lxml')
+
+    ccc = soup.find_all(class_='product-item__link')
+    sss.extend(ccc)
+    break
+
+chasy = []
+
+for i in sss:
+    soup = BeautifulSoup(i.text, 'lxml')
+    name = soup.find('p').text.split()[1]
+    cena = soup.find('p').text.split()[-2] + ' ' + soup.find('p').text.split()[-1] + soup.find('p').text.split()[-3]
+    url = 'https://shop.casio.ru'+i.get('href')
+    dd = [name, cena, url]
+    chasy.append(dd)
+
+
+with open('chasy/sss.json', 'w') as f:
+    json.dump(chasy, f, indent=4,ensure_ascii=False)
